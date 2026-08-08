7c1aca4d0f981a75b2713df5681accab380ca1d3
Akshaya R <rakshay5764@gmail.com>
2026-08-08 13:58:48 +0530

    Session 22: Remove manual refresh buttons on reports + notifications, delete legacy placeholder page

--- frontend/src/app/notifications/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Page() {
-  return (
-    <div className="p-8">
-      <h1 className="text-gray-900 dark:text-[#e6edf3]">Coming Soon</h1>
-    </div>
-  );
-}
